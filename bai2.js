6936d1360ca87e550a49fe4a6d62fc9b00b2149b
lexuanhieu200001 <lexuanhieu200001@gmail.com>
2023-11-09 16:46:44 +0700

    nop bai tap

---
+++ bai2.js
@@ -0,0 +1,4 @@
+let width=20;
+let height = 20;
+let chieucao = width * height;
+document.write('chieucao=' +chieucao);
---
+++ bai3.js
@@ -0,0 +1,5 @@
+let a=prompt('Nhập số a');
+let b=prompt('Nhập số b');
+a = parseInt(a);
+b = parseInt(b);
+document.write("Phần dư khi chia hai số" + a + "và" + b + "là"+ (a%b)+"</br>");
